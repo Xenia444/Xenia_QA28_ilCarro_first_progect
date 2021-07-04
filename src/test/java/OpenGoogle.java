160119d9001c03febe14cf987114398b92ba9637
Xenia444 <86400654+Xenia444@users.noreply.github.com>
2021-07-04 20:55:56 +0300

    added new classes

--- src/test/java/OpenGoogle.java
+++ src/test/java/OpenGoogle.java
@@ -1,8 +1,9 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -10,23 +11,27 @@ import org.testng.annotations.Test;
 public class OpenGoogle {
 
     WebDriver wd;
-
+    WebDriverWait wait;
     @BeforeMethod
+
     public void setUp() {
         wd = new ChromeDriver();
+        wait = new WebDriverWait(wd, 20);
         wd.manage().window().maximize();
         wd.get("https://www.google.com/");
     }
 
     @Test
     public void searchWebDriver() {
-        wd.findElement(By.name("q")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("q"))).click();
+       // wd.findElement(By.name("q")).click();
         wd.findElement(By.name("q")).clear();
         wd.findElement(By.name("q")).sendKeys("WebDriver" + Keys.ENTER);
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(10000);
         wd.quit();
     }
 }
